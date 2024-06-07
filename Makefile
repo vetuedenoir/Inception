2d9517cafb6dc04cc49e4b608370483b9df4ca01
vetuedenoir <killianscordel@gmail.com>
2024-06-07 21:23:48 +0200

    vraisemblablement ca ne marche pas

--- Makefile
+++ Makefile
@@ -1,2 +1,27 @@
-le makefile
+#lol
 
+all:
+	mkdir -p /home/kscordel/data/mariadb
+	mkdir -p /home/kscordel/data/wordpress
+	docker compose -f ./srcs/docker-compose.yml build
+	docker compose -f ./srcs/docker-compose.yml up -d
+
+logs:
+	docker logs wordpress
+	docker logs mariadb
+	docker logs nginx
+
+clean:
+	docker container stop nginx mariadb wordpress
+#	docker network rm inception
+
+fclean: clean
+	sudo rm -rf /home/kscordel/data/mariadb/*
+	sudo rm -rf /home/kscordel/data/wordpress/*
+	docker volume rm srcs_mariadb
+	docker volume rm srcs_wordpress
+	@docker system prune -af
+
+re: fclean all
+
+.Phony: all logs clean fclean
--- srcs/requirements/mariadb/tools/db_init_script.sh
+++ srcs/requirements/mariadb/tools/db_init_script.sh
@@ -1,15 +1,36 @@
 #!bin/bash
 
+#service mariadb start;
+
+#mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\';"
+#mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' INDENTIFIED BY '${SQL_PASSWORD}';"
+#mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' INDENTIFIED BY '${SQL_PASSWORD}';"
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+#mysql -e "FLUSH PRIVILEGES;"`
+
+#mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+
+#exec mysqld_safe
+
+#echo "Mariadb user and database created succesfully"
+
+
+#!/bin/bash
+#set -eux
+
 service mariadb start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\';"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' INDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' INDENTIFIED BY '${SQL_PASSWORD}';"
+# log into MariaDB as root and create database and the user
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" 
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+#mysqladmin -u root shutdown
 exec mysqld_safe
 
-echo "Mariadb user and database created succesfully"
+#print status
+echo "MariaDB database and user were created successfully! "
